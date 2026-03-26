503f47f62821993cecdb5f75581f611cc5e8f6f2
MiguelT-R-M <miguelhrs1@hotmail.com>
2026-03-26 11:41:51 -0300

    Update to be dynamic

--- Matrix_Mult/Matrix_mult.py
+++ Matrix_Mult/Matrix_mult.py
@@ -1,13 +1,31 @@
 
-## MA = int(input("Informe a quantidade de linhas da Mat A ")) 
-## NA = int(input("Informe a Colunas de linhas da Mat A "))
+MA = int(input("Informe a quantidade de linhas da Mat A ")) 
+NA = int(input("Informe a Colunas de linhas da Mat A "))
+print("")
+MB = int(input("Informe a quantidade de linhas da Mat B "))
+NB = int(input("Informe a Colunas de linhas da Mat B "))
 
-## MB = int(input("Informe a quantidade de linhas da Mat B "))
-## NB = int(input("Informe a Colunas de linhas da Mat B "))
+Mat_A = []
+Mat_B = []
+Mat_mult = []
 
-Mat_A =    [[10, 58, 74],[89, 11, 11],[58, 74, 96]]
-Mat_B =    [[22, 41, 13],[29, 10, 9],[7, 1, 2]]
-Mat_mult = [[0, 0 ,0],[0, 0, 0],[0, 0, 0]]
+print("")
+for x in range(0, MA):
+    Mat_A.append([])
+    for y in range(0, NA):
+        Mat_A[x].append(int(input(f"Informe a posição ({x+1},{y+1}) da matriz A: ")))
+print("")
+
+for x in range(0, MB):
+    Mat_B.append([])
+    for y in range(0, NB):
+        Mat_B[x].append(int(input(f"Informe a posição ({x+1},{y+1}) da matriz B: ")))
+
+for x in range(0, MA):
+    Mat_mult.append([])
+    for y in range(0, NB):
+        Mat_mult[x].append(0)
+
 
 if(len(Mat_A[0])==len(Mat_B)):
 
@@ -15,7 +33,8 @@ if(len(Mat_A[0])==len(Mat_B)):
 
         for y in range(0, len(Mat_B[0])):
 
-            for z in range(0, len(Mat_B[0])):
+            for z in range(0, len(Mat_B)):
+                
                 Mat_mult[x][y] += Mat_A[x][z]*Mat_B[z][y]
 
 for x in Mat_mult:
