68938e7a32249ca5a03fc9d2e0c49420fcf872ca
MiguelT-R-M <miguelhrs1@hotmail.com>
2026-04-15 19:53:43 -0300

    Updates!

--- Self_aproximation/Self_aproximation.c
+++ Self_aproximation/Self_aproximation.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
+#define isValid(Choices, Current) ((Choices[0] == Current[0])&&(Choices[1] == Current[1])&&(Choices[2] == Current[2])&&(Current[3]>=Choices[3])&&(Current[3]<=Choices[4]))
 
 void checkNull(void *pointer){
     if(pointer == NULL){
@@ -14,6 +15,13 @@ void checkNull(void *pointer){
 int main(void){
 
     int Loc_size = 1, Capacity = 10;
+
+    long double Choices[5] = {0}, Current[4] = {0};
+    printf("Escolha o ano desejado: "); scanf("%Lf", &Choices[0]);
+    printf("Escolha o mês desejado: "); scanf("%Lf", &Choices[1]);
+    printf("Escolha o dia desejado: "); scanf("%Lf", &Choices[2]);
+    printf("Escolha a hora inicial: "); scanf("%Lf", &Choices[3]);
+    printf("Escolha a hora final: "); scanf("%Lf", &Choices[4]);
 
     FILE *locale = fopen("locale.txt", "r");
     checkNull(locale);
@@ -27,31 +35,39 @@ int main(void){
 
     while(fgets(BUFFER, 100, locale)){
         
-        Loc_X[Loc_size-1] = strtold(BUFFER + 34, &garbage);
+        Current[0] = strtold(BUFFER + 6, &garbage);
+        Current[1] = strtold(garbage + 1, &garbage);
+        Current[2] = strtold(garbage + 1, &garbage);
+        Current[3] = strtold(garbage + 1, &garbage);
+        Loc_X[Loc_size-1] = strtold(garbage + 15, &garbage);
         Loc_Y[Loc_size-1] = strtold(garbage + 1, &garbage);
 
-        Sum_locx+=Loc_X[Loc_size-1]; Sum_locy+=Loc_Y[Loc_size-1];
+        if(isValid(Choices, Current)){
+            Sum_locx+=Loc_X[Loc_size-1]; Sum_locy+=Loc_Y[Loc_size-1];
 
-        printf("%.7Lf, %.7Lf\n", Loc_X[Loc_size-1], Loc_Y[Loc_size-1]);
+            printf("%.7Lf, %.7Lf\n", Loc_X[Loc_size-1], Loc_Y[Loc_size-1]);
 
-        Loc_size++;
+            Loc_size++;
 
-        if(Loc_size-1>=Capacity){
+            if(Loc_size-1>=Capacity){
 
-            Capacity = Capacity * 2;
-            void *tempX = realloc(Loc_X, Capacity * sizeof(*Loc_X)); void *tempY = realloc(Loc_Y, Capacity * sizeof(*Loc_Y));
-            checkNull(tempX); checkNull(tempY);
-            Loc_X = tempX; Loc_Y = tempY;
+                Capacity = Capacity * 2;
+                void *tempX = realloc(Loc_X, Capacity * sizeof(*Loc_X)); void *tempY = realloc(Loc_Y, Capacity * sizeof(*Loc_Y));
+                checkNull(tempX); checkNull(tempY);
+                Loc_X = tempX; Loc_Y = tempY;
 
+            }
+        }else{
+            printf("Localização inválida, checando próxima\n");
         }
 
     }
-
+    printf("Fim dos testes\n");
     printf("\n");
     Avg_locx = Sum_locx/(Loc_size-1); Avg_locy = Sum_locy/(Loc_size-1);
 
-    printf("Média de X: %.7Lf, Média de Y: %.7Lf\n", Avg_locx, Avg_locy);
-    printf("%.7Lf, %.7Lf\n", Avg_locx, Avg_locy);
+    printf("Média de X: %.7Lf, Média de Y: %.7Lf\n\n", Avg_locx, Avg_locy);
+    printf("%.7Lf, %.7Lf\n\n", Avg_locx, Avg_locy);
 
     free(Loc_X); free(Loc_Y); fclose(locale);
     return 0;
